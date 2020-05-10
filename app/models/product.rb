6d33e01d0b140c4a6b180d5758dd3853c8e493a5
Topher000 <kking@execonline.com>
2020-05-09 23:48:41 -0600

    Update for work week

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
     validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
     validates :title, length: {minimum: 10, message: 'must be at least 10 characters long' }
@@ -7,4 +10,13 @@ class Product < ApplicationRecord
         message: 'must be a URL for gif, jpg or png image.'
     }
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    private
+        #ensure that there are no line items referencing this product
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
 end
