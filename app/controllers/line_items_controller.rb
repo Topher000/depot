71c3d793526bf0a97dc7db955354e5bce315d0a4
Topher000 <kking@execonline.com>
2020-05-13 10:16:04 -0600

    Depot updates

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,15 +1,19 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
   # GET /line_items.json
   def index
     @line_items = LineItem.all
+   
   end
 
   # GET /line_items/1
   # GET /line_items/1.json
   def show
+    
   end
 
   # GET /line_items/new
@@ -24,11 +28,15 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    session[:visit_count] = 0
+  
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, 
+          notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -49,6 +57,7 @@ class LineItemsController < ApplicationController
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
+    
   end
 
   # DELETE /line_items/1
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    session[:visit_count] ||= 0 #initalize the vist count on first visit
+    session[:visit_count] += 1 # increment the count with each visit
+    @visit_count = session[:visit_count]
   end
 end
